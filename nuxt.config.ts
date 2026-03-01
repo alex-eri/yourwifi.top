3b8cd2c913c6c70fcc0e60b4bf502e5436e2cd46
Степанов Александр <sa@eri.su>
2026-03-01 09:39:42 +0300

    Setup Pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,7 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
 
   routeRules: {
-    '/': { prerender: false }
+    '/': { prerender: true }
   },
 
   compatibilityDate: '2025-01-15',
